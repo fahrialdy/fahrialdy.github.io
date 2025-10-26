d8b8ea4f51b3bceca01b0c8878e86b6b0473010b
fahrialdy <fahrialdyfebrians@gmail.com>
2025-10-26 17:37:06 +0700

    Delete webdasar.com directory

--- webdasar.com/includes/navbar.php
+++
@@ -1,7 +0,0 @@
-<nav>
-    <ul>
-        <li><a href="index.php">Beranda</a></li>
-        <li><a href="article.php">Artikel</a></li>
-        <li><a href="profil.php">Profil</a></li>
-    </ul>
-</nav>
